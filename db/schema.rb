14418c361b00fc8d8fcda453d20567cdb6bbc9ba
davidhurley <dhurls99@gmail.com>
2014-06-23 14:54:15 -0400

    allow uers to sign up, sign in, or logout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623143851) do
+ActiveRecord::Schema.define(version: 20140623184514) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140623143851) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.string   "name"
   end
 
 end
